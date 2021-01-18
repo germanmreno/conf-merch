25b25792a6220ffc08bcff1ee05c99ef9ce57a09
Germán Moreno <grmmrno@gmail.com>
2021-01-18 19:34:08 -0400

    Add React Helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import AppContext from '../context/AppContext';
 import handleSumTotal from '../helpers/handleSumTotal';
@@ -19,31 +20,36 @@ const Checkout = () => {
   const total = handleSumTotal(cart);
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? <h3>Order car:</h3> : <h3>Car empty</h3>}
+    <>
+      <Helmet>
+        <title>Order list - Conf Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? <h3>Order car:</h3> : <h3>Car empty</h3>}
 
-        {cart.map((item) => (
-          <div className="Checkout-item" key={item.ref}>
-            <div className="Checkout-element">
-              <h4>{item.title}:</h4>
-              <span>${item.price}</span>
+          {cart.map((item) => (
+            <div className="Checkout-item" key={item.ref}>
+              <div className="Checkout-element">
+                <h4>{item.title}:</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(item)}>
+                <i className="fas fa-trash-alt" />
+              </button>
             </div>
-            <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash-alt" />
-            </button>
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Total price: $ ${total}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continue </button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Total price: $ ${total}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continue </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Products from '../components/Products';
 
-const Home = () => <Products />;
+const Home = () => (
+  <>
+    <Helmet>
+      <title>Conf Merch - Products</title>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@grmmoreno" />
+      <meta name="twitter:creator" content="@grmmoreno" />
+      <meta name="twitter:title" content="Conf Store" />
+      <meta name="twitter:description" content="Conf Store" />
+      <meta
+        name="twitter:image"
+        content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+      />
+    </Helmet>
+    <Products />
+  </>
+);
 
 export default Home;
